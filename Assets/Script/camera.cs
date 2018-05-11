ffa35da67e9a9764277d9e75e966852a4ffcfd4c
WIN10OHS50261\LEESHINHEE <wmfehdgml@gmail.com>
2018-05-12 00:04:29 +0900

    block setting

---
+++ Assets/Script/camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+    public Camera maincamera;
+    public GameObject cm;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
