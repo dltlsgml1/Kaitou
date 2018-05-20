b038d33f3b2a1ebe1da4adbc0814e22ebe607993
LEESHINHEE-DESK\dltlsgml1 <wmfehdgml@gmail.com>
2018-05-20 23:24:38 +0900

    作業中（仮）

--- Assets/Script/GameMain.cs
+++ Assets/Script/GameMain.cs
@@ -10,6 +10,7 @@ public class GameMain : MonoBehaviour {
     public GameObject[] CollapsBlock = new GameObject[10];
     public GameObject[] WetBlock = new GameObject[10];
     public Camera MainCamera;
+    public GameObject Box;
     Ray ray1, ray2;
 
     public int BlocksCount =0;
@@ -45,34 +46,105 @@ public class GameMain : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetMouseButtonDown(0))
+
+        
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Block[0].gameObject.GetComponent<Blocks>().BurnFlg = true;
+        //    Block[0].gameObject.GetComponent<Blocks>().SetBurn(Block[0]);
+
+        //    CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().BurnFlg = true;
+        //    CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().SetBurn(CollapsBlock[0]);
+
+
+        //}
+
+
+        Atari();
+    }
+
+    void Atari()
+    {
+        Mesh CollapsMesh = CollapsBlock[0].GetComponent<MeshFilter>().mesh;
+        Mesh BlockMesh = Block[0].GetComponent<MeshFilter>().mesh;
+
+        Vector3[] CollapsVertices = CollapsMesh.vertices;
+        Vector3[] BlockVertices = BlockMesh.vertices;
+
+
+        for(int i=0;i<8;i++)
+        {
+            CollapsVertices[i] = MainCamera.WorldToScreenPoint(CollapsVertices[i]);
+            BlockVertices[i] = MainCamera.WorldToScreenPoint(BlockVertices[i]);
+
+        }
+
+        if(
+            Vector3.Distance(CollapsVertices[3],BlockVertices[2])<15.0f &&
+            Vector3.Distance(CollapsVertices[5],BlockVertices[4])<15.0f &&
+            Vector3.Distance(CollapsVertices[7],BlockVertices[6])<15.0f &&
+            Vector3.Distance(CollapsVertices[1],BlockVertices[0])<15.0f 
+            )
         {
             Block[0].gameObject.GetComponent<Blocks>().BurnFlg = true;
             Block[0].gameObject.GetComponent<Blocks>().SetBurn(Block[0]);
 
             CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().BurnFlg = true;
             CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().SetBurn(CollapsBlock[0]);
-
-
         }
-        Atari();
-    }
+        if (
+            Vector3.Distance(CollapsVertices[2], BlockVertices[3]) < 15.0f &&
+            Vector3.Distance(CollapsVertices[4], BlockVertices[5]) < 15.0f &&
+            Vector3.Distance(CollapsVertices[6], BlockVertices[7]) < 15.0f &&
+            Vector3.Distance(CollapsVertices[0], BlockVertices[1]) < 15.0f
+            )
+        {
+            Block[0].gameObject.GetComponent<Blocks>().BurnFlg = true;
+            Block[0].gameObject.GetComponent<Blocks>().SetBurn(Block[0]);
 
-    void Atari()
-    {
-        Vector3 tempvec= CollapsBlock[0].gameObject.transform.position;
-        Vector3 tempvec2 = MainCamera.WorldToScreenPoint(tempvec);
-        Debug.Log(CollapsBlock[0].gameObject.transform.position);
-        Debug.Log(tempvec2);
+            CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().BurnFlg = true;
+            CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().SetBurn(CollapsBlock[0]);
+        }
 
-        ray1.origin = Block[0].transform.position;
-        ray1.direction = MainCamera.transform.position;
-        ray2.origin = CollapsBlock[0].transform.position;
-        ray2.direction = MainCamera.transform.position;
-        Debug.DrawRay(ray1.origin, ray1.direction * 100, Color.red);
-        Debug.DrawRay(ray2.origin, ray2.direction * 100, Color.red);
+        //if (
+        //   Vector3.Distance(CollapsVertices[3], BlockVertices[5]) < 1.0f &&
+        //   Vector3.Distance(CollapsVertices[2], BlockVertices[4]) < 1.0f &&
+        //   Vector3.Distance(CollapsVertices[1], BlockVertices[7]) < 1.0f &&
+        //   Vector3.Distance(CollapsVertices[0], BlockVertices[6]) < 1.0f
+        //   )
+        //{
+        //    Block[0].gameObject.GetComponent<Blocks>().BurnFlg = true;
+        //    Block[0].gameObject.GetComponent<Blocks>().SetBurn(Block[0]);
+
+        //    CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().BurnFlg = true;
+        //    CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().SetBurn(CollapsBlock[0]);
 
 
+        //}
+        //if (
+        //   Vector3.Distance(CollapsVertices[3], BlockVertices[1]) < 1.0f &&
+        //   Vector3.Distance(CollapsVertices[2], BlockVertices[0]) < 1.0f &&
+        //   Vector3.Distance(CollapsVertices[5], BlockVertices[7]) < 1.0f &&
+        //   Vector3.Distance(CollapsVertices[4], BlockVertices[6]) < 1.0f
+        //   )
+        //{
+        //    Block[0].gameObject.GetComponent<Blocks>().BurnFlg = true;
+        //    Block[0].gameObject.GetComponent<Blocks>().SetBurn(Block[0]);
+
+        //    CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().BurnFlg = true;
+        //    CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().SetBurn(CollapsBlock[0]);
+
+
+        //}
+
+        Debug.Log(Vector3.Distance(CollapsVertices[4], BlockVertices[4]));
+        Debug.Log(CollapsVertices[4]);
+        Debug.Log(BlockVertices[4]);
+        Debug.Log(CollapsBlock[0].transform.position);
+        Debug.Log(Block[0].transform.position);
+        Debug.Log(MainCamera.WorldToScreenPoint(CollapsBlock[0].transform.position));
+        Debug.Log(MainCamera.WorldToScreenPoint(Block[0].transform.position));
 
     }
 
