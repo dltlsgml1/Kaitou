50bf7cec10e26410038475e4015c1aa0b0dad08c
WIN10OHS50261\LEESHINHEE <wmfehdgml@gmail.com>
2018-05-22 10:47:16 +0900

    ブロック全体での燃え移り判定　完成（要検証）

--- Assets/Script/GameMain.cs
+++ Assets/Script/GameMain.cs
@@ -14,7 +14,7 @@ public class GameMain : MonoBehaviour {
     public GameObject[] bk = new GameObject[10];
     public GameObject[] ck = new GameObject[10];
 
-
+    public int CollapsBlockCount = 0;
     public int BlocksCount = 0;
     public int NowStage = 0;
 
@@ -40,7 +40,7 @@ public class GameMain : MonoBehaviour {
         Block = GameObject.FindGameObjectsWithTag("NormalBlock");
         CollapsBlock = GameObject.FindGameObjectsWithTag("CollapseBlock");
         WetBlock = GameObject.FindGameObjectsWithTag("WetBlock");
-
+        
     }
 
     
@@ -68,29 +68,43 @@ public class GameMain : MonoBehaviour {
 
         Vector3[] bkvec = new Vector3[6];
         Vector3[] ckvec = new Vector3[6];
-        for (int i = 0; i < 6; i++)
-        {
-            bk[i] = Block[0].transform.GetChild(i).gameObject;
-            ck[i] = CollapsBlock[0].transform.GetChild(i).gameObject;
-            bkvec[i] = bk[i].transform.position;
-            ckvec[i] = ck[i].transform.position;
+        for (int i = 0; i < CollapsBlock.Length; i++)
+        {      
+            for (int j=0;j<Block.Length;j++)
+            {
+                for (int k=0;k<6;k++)
+                {
+                    ck[k] = CollapsBlock[i].transform.GetChild(k).gameObject;
+                    ckvec[k] = MainCamera.WorldToScreenPoint(ck[k].transform.position);
+                    for (int l = 0; l < 6;l++)
+                    {
+                        bk[l] = Block[j].transform.GetChild(l).gameObject;   
+                        bkvec[l] = MainCamera.WorldToScreenPoint(bk[l].transform.position);
+                        if(Vector3.Distance(ckvec[k],bkvec[l])<5.0f)
+                        {
+                            Block[j].gameObject.GetComponent<Blocks>().BurnFlg = true;
+                            Block[j].gameObject.GetComponent<Blocks>().SetBurn(Block[0]);
+                            CollapsBlock[i].gameObject.GetComponent<CollapseBlock>().BurnFlg = true;
+                            CollapsBlock[i].gameObject.GetComponent<CollapseBlock>().SetBurn(CollapsBlock[0]);
+                        }
 
-            bkvec[i] = MainCamera.WorldToScreenPoint(bkvec[i]);
-            ckvec[i] = MainCamera.WorldToScreenPoint(ckvec[i]);
-
+                    }
+                }
+            }
+           
         }
 
-       
-        if(Vector3.Distance(bkvec[(int)DefineScript.CollisionIndex.Right],ckvec[(int)DefineScript.CollisionIndex.Left])<5.0f)
-        {
 
-                Block[0].gameObject.GetComponent<Blocks>().BurnFlg = true;
-                Block[0].gameObject.GetComponent<Blocks>().SetBurn(Block[0]);
+        //if (Vector3.Distance(bkvec[(int)DefineScript.CollisionIndex.Right],ckvec[(int)DefineScript.CollisionIndex.Left])<5.0f)
+        //{
 
-                CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().BurnFlg = true;
-                CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().SetBurn(CollapsBlock[0]);
+        //        Block[0].gameObject.GetComponent<Blocks>().BurnFlg = true;
+        //        Block[0].gameObject.GetComponent<Blocks>().SetBurn(Block[0]);
 
-        }
+        //        CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().BurnFlg = true;
+        //        CollapsBlock[0].gameObject.GetComponent<CollapseBlock>().SetBurn(CollapsBlock[0]);
+
+        //}
 
         Debug.Log(bkvec[(int)DefineScript.CollisionIndex.Top]);
         Debug.Log(ckvec[(int)DefineScript.CollisionIndex.Top]);
