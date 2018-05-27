9383c0fbdba64ce95a680af17b882eff641e02fc
ikeda <shinto090205@gmail.com>
2018-05-27 22:01:29 +0900

    exe版一応動作は完成。 スクリーンショットの上書きにうまくいっていないとこがあるので以後修正。

--- Assets/Script/ScreenShot.cs
+++ Assets/Script/ScreenShot.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿/*
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -92,4 +93,163 @@ public class ScreenShot : MonoBehaviour {
 
     }
 
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    private string projectName;
+
+    private string saveFilePath;
+    private string fileName;
+
+    private int stageMax = 3;
+
+    private int stageId;
+    private bool isRunning;
+
+    // Use this for initialization
+    void Start()
+    {
+        projectName = "test";
+        saveFilePath = CreateSavePath(projectName);
+
+        stageId = 0;
+
+        isRunning = false;
+
+        StartCoroutine(CreateScreenshot(stageId));
+
+        for (int i = 1; i <= stageMax; i++)
+        {
+            SearchToSetCrearImage(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // スクリーンショット作成
+            StartCoroutine(CreateScreenshot(stageId));
+            // マテリアルの更新
+            StartCoroutine(LoadClearImageToMaterial(stageId));
+        }
+        keyNum();
+
+    }
+
+    void keyNum()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            stageId = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            stageId = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            stageId = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            stageId = 3;
+        }
+    }
+
+    IEnumerator CreateScreenshot(int id)
+    {
+        if (System.IO.File.Exists("clear_stage" + id + ".png") == true)
+        {
+            // ファイル削除
+            System.IO.File.Delete("clear_stage" + id + ".png");
+            while (System.IO.File.Exists("clear_stage" + id + ".png") == true)
+            {
+                yield return null;
+            }
+        }
+
+        // スクリーンショットを撮る
+        ScreenCapture.CaptureScreenshot("clear_stage" + id + ".png");
+
+
+        while (System.IO.File.Exists("clear_stage" + id + ".png") == false)
+        {
+            yield return null;
+        }
+
+    }
+
+    IEnumerator LoadClearImageToMaterial(int id)
+    {
+        if (isRunning)
+        {
+            yield return null;
+        }
+        isRunning = true;
+
+        while (System.IO.File.Exists("test_Data/" + "clear_stage" + id + ".png") == false)
+        {
+            yield return null;
+        }
+        // ①．ファイル => バイナリ変換
+        byte[] image = System.IO.File.ReadAllBytes("test_Data/" + "clear_stage" + id + ".png");
+
+        // ②．受け入れ用Texture2D作成
+        Texture2D tex = new Texture2D(0, 0);
+
+        // ③．バイナリ => Texture変換
+        tex.LoadImage(image);
+
+        // ④．Texture2Dをマテリアルに指定
+        MeshRenderer renderer = GameObject.Find("clear_stage" + id).GetComponent<MeshRenderer>();
+        renderer.materials[0].mainTexture = tex;
+
+        isRunning = false;
+    }
+
+    void SearchToSetCrearImage(int id)
+    {
+        if (System.IO.File.Exists("test_Data/" + "clear_stage" + id + ".png") == true)
+        {
+            // ①．ファイル => バイナリ変換
+            byte[] image = System.IO.File.ReadAllBytes("test_Data/" + "clear_stage" + id + ".png");
+
+            // ②．受け入れ用Texture2D作成
+            Texture2D tex = new Texture2D(0, 0);
+
+            // ③．バイナリ => Texture変換
+            tex.LoadImage(image);
+
+            // ④．Texture2Dをマテリアルに指定
+            MeshRenderer renderer = GameObject.Find("clear_stage" + id).GetComponent<MeshRenderer>();
+            renderer.materials[0].mainTexture = tex;
+
+        }
+    }
+
+    // 保存先
+    private string CreateSavePath(string projectname)
+    {
+        return projectname + "_Data/";
+    }
+
+    // ステージIDの名前作成(クリア用)
+    private string CreateClearFilename()
+    {
+        return "clear_stage";
+    }
+
+    // ステージIDのファイル名作成(クリア用)
+    private string CreatePngFilename(int id)
+    {
+        return CreateClearFilename() + id + ".png";
+    }
 }
