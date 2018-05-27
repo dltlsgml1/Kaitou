069b7f11a8d00906dfe11b39e711e9996fb073b7
ikeda <shinto090205@gmail.com>
2018-05-27 15:55:37 +0900

    unityエディタ上でのみ動くもの完成。 exe形式用のものはまだ。

--- Assets/Script/ScreenShot.cs
+++ Assets/Script/ScreenShot.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class ScreenShot : MonoBehaviour {
 
     public string savePath = "Assets/Resources/StageSelect/SS_Canvas/ClearStageSS/";
+    //public string savePath = "";
 
     public string fileName = "clear_stage";
     public int count;
 
-    //bool isRunning = false;
+    bool isRunning = false;
 
 	// Use this for initialization
 	void Start () {
@@ -25,31 +27,69 @@ public class ScreenShot : MonoBehaviour {
         {
             count++;
 
-            CreateClearImage();
-            LoadClearImageToMaterial("StageSelect/SS_Canvas/ClearStageSS/");
+            //CreateClearImage();
+            //LoadClearImageToMaterial("StageSelect/SS_Canvas/ClearStageSS/");
+
+            StartCoroutine("CreateClearImage");
+
         }
 
     }
 
-    void CreateClearImage()
+    IEnumerator CreateClearImage()
     {
+        // 並行しない用処理
+        if (isRunning)
+        {
+            yield return null;
+        }
+        isRunning = true;
+
+        // 保存先
         var texturePath = savePath + fileName + count + ".PNG";
 
+        // 前のスクリーンショットを削除
+        while(System.IO.File.Exists(texturePath))
+        {
+            System.IO.File.Delete(texturePath);
+        }
+
+        // スクリーンショット作成
         ScreenCapture.CaptureScreenshot(texturePath);
 
-        Debug.Log("createSS: " + texturePath);
+        yield return new WaitForEndOfFrame(); 
 
+        // ファイルの生成確認
+        while(!System.IO.File.Exists(texturePath))
+        {
+            Debug.Log("not yet SS");
+            yield return null;
+        }
+        Debug.Log("createSS: " + fileName + count);
+
+        // unityのアセットフォルダの更新
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        AssetDatabase.ImportAsset(texturePath);
+
+        yield return new WaitForEndOfFrame();
+
+        // マテリアルの更新
+        LoadClearImageToMaterial("StageSelect/SS_Canvas/ClearStageSS/");
+
+        // 終了処理
+        isRunning = false;
     }
 
     void LoadClearImageToMaterial(string path)
     {
-        GameObject obj = Instantiate(Resources.Load(path + "Models/" + fileName + 1)) as GameObject;
-        //obj.renderer.material.mainTexture = Resources.Load("image") as Texture2D;
-        obj.GetComponent<Renderer>().material.mainTexture = Resources.Load(path + fileName + count) as Texture2D;
+        // テクスチャロード
+        Texture2D clearTex = Resources.Load(path + fileName + count) as Texture2D;
 
-        //Material mat = Resources.Load(path + "Material/" + fileName + count) as Material;
+        // セットしたいオブジェクトにロードしたテクスチャをセット
+        GameObject obj = GameObject.Find(fileName + count);
+        obj.GetComponent<Renderer>().material.SetTexture("_MainTex", clearTex);
 
-        //mat.mainTexture = Resources.Load(path + fileName + count) as Texture2D;
     }
 
 }
