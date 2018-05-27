3ad2a5a3b7bc0049dd0c4c8419c996e08fa48171
nieda <ayaki.nieda@gmail.com>
2018-05-27 18:43:07 +0900

    カーソル、決定実装

--- Assets/Script/Pause.cs
+++ Assets/Script/Pause.cs
@@ -10,34 +10,46 @@ public class Pause : MonoBehaviour {
     //　ポーズした時に表示するUI
     [SerializeField]
     private GameObject pauseUI;
-
+    GameObject Cursor;
+    PauseSelect pauseselect;
 
 
 	// Use this for initialization
 	void Start () {
-       // gameObject.SetActive(true); //活性化
-        
-            //ゲームタイム維持
+   
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        //ｑキーでゲームバック
-        BackGameMain();
-
-
-        //ｒキーでリスタート
-        if (Input.GetKeyDown("r"))
+        ////ｑキーでゲームバック
+        if (Input.GetKeyDown("q"))
         {
-            Restart();
+             BackGameMain();
         }
 
-        //spaceキーでステセレ遷移
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && is_pause)  //決定キーに差し替え予定
         {
-            BackStageSelect();
-        }      
+            this.Cursor = GameObject.Find("CameraObejct/Pause/Pause_Cursor");
+            this.pauseselect = Cursor.GetComponent<PauseSelect>();
+
+            switch (pauseselect.move)
+            {
+                case 0:
+                    //SE追加予定
+                    BackGameMain();
+                    break;
+                case 1:
+                    //SE追加予定
+                    Restart();
+                    break;
+                case 2:
+                    //SE追加予定
+                    BackStageSelect();
+                    break;
+
+            }
+        }
 
         //gameObject.SetActive(false); //非活性化
 
@@ -75,8 +87,8 @@ public class Pause : MonoBehaviour {
         //アニメーション追加予定
         //SE追加予定
    
-        if (Input.GetKeyDown("q"))
-        {
+        //if (Input.GetKeyDown("q"))
+        //{
             //　ポーズUIのアクティブ、非アクティブを切り替え
             pauseUI.SetActive(!pauseUI.activeSelf);
 
@@ -92,7 +104,7 @@ public class Pause : MonoBehaviour {
                 Time.timeScale = 1f;
                 is_pause = false;
             }
-        }
+        //}
 
     }
 
