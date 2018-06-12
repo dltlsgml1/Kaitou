78f6ce276e22bd54af84b2c589b67005d2f32056
NotePC_Cielo <sorara.sky231@gmail.com>
2018-06-12 19:32:02 +0900

    Bloomの追加/タイトルからのフェードを実装

--- Assets/Script/Title.cs
+++ Assets/Script/Title.cs
@@ -1,21 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour {
+    public GameObject tt_Faield;
+    private failed cs_failed;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        cs_failed = tt_Faield.GetComponent<failed>();
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         //マウスクリックで任意のシーン切り替え
         if (Input.GetButtonDown("BButton"))
         {
-            SceneManager.LoadScene("StageSelect", LoadSceneMode.Single);
+            cs_failed.FadeIn_On();
+           
+        }
+        if (cs_failed.FadeInEnd)
+        {
+            SceneManager.LoadScene("StageSelect_Temp", LoadSceneMode.Single);
         }
 
     }
