ac21c0cee372e431ce4a5ec46629818b987e063e
ikeda <shinto090205@gmail.com>
2018-06-14 20:26:48 +0900

    額縁適応。

---
+++ Assets/Script/StageRank.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageRank : MonoBehaviour {
+
+    private RANK Rank;
+
+    public enum RANK{
+        NORMAL = 0,
+        BRONZE,
+        SILVER,
+        GOLD,
+    }
+
+    public void CheckRank(int id)
+    {
+        RANK sts;
+
+        int clearCnt = (int)CSVData.StageDateList[id].ClearFlag;
+        int minCnt = (int)CSVData.StageDateList[id].MinCunt;
+        int upperCnt = (int)CSVData.StageDateList[id].UpperCunt;
+        float minTmp = clearCnt - minCnt;
+        float limTmp = (upperCnt - minCnt) / 2;
+        
+        if(minTmp < 0)
+        {
+            sts = RANK.NORMAL;
+        }
+        else if(minTmp == 0)
+        {
+            sts = RANK.GOLD;
+        }
+        else if (minTmp <= limTmp && clearCnt != upperCnt)
+        {
+            sts = RANK.SILVER;
+        }
+        else
+        {
+            sts = RANK.BRONZE;
+        }
+
+        SetRank(sts);
+        
+    }
+
+    private void SetRank(RANK rank)
+    {
+        Rank = rank;
+    }
+
+    public RANK GetRank()
+    {
+        return Rank;
+    }
+}
