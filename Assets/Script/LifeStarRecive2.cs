9ca081199629953001bf75a12cc34c25f8713b9a
WIN10OHS50261\LEESHINHEE <wmfehdgml@gmail.com>
2018-06-19 10:04:59 +0900

    上限回数表現の改良版実装中（未完成）

---
+++ Assets/Script/LifeStarRecive2.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeStarRecive2 : MonoBehaviour {
+    public int ReceiveLimitNum;
+    public bool clearflg;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
