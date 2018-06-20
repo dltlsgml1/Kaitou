4bfb1080d8cb0d284f59d6c79c5dc37dd92cba06
WIN8OHS50173\ohs50173 <keisuke.877877@gmail.com>
2018-06-20 17:11:36 +0900

    線発光から星発光に変更

--- Assets/Script/StageStar2.cs
+++ Assets/Script/StageStar2.cs
@@ -4,12 +4,45 @@ using UnityEngine;
 
 public class StageStar2 : MonoBehaviour {
 
-    //public GameObject SetStarObj;
-    LifeStarRecive2 GetLimitStar;
+    public struct light_container
+    {
+        public float light;
+        public bool lightup;
+    }
+    public Material[] lightmat = new Material[10];
+    //public GameObject SetStarlineObj;
+    LifeStarRecive2 GetLimitStarLine;
+    light_container[] Lcontainer = new light_container[10];
+
+
+
     // Use this for initialization
     void Start()
     {
-        GetLimitStar = GetComponentInParent<LifeStarRecive2>();
+
+        GetLimitStarLine = GetComponentInParent<LifeStarRecive2>();
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            Lcontainer[i].light = 0.0f;
+            Lcontainer[i].lightup = false;
+        }
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            this.transform.GetChild(i).GetComponent<MeshRenderer>().material = lightmat[i];
+            if (i < GetLimitStarLine.ReceiveLimitNum+1)
+            {
+                this.transform.GetChild(i).gameObject.SetActive(true);
+                Lcontainer[i].light = 1.0f;
+
+            }
+            else
+            {
+                this.transform.GetChild(i).gameObject.SetActive(false);
+                Lcontainer[i].light = -0.5f;
+            }
+            Lcontainer[i].lightup = true;
+            lightmat[i].SetColor("_EmissionColor", new Color(Lcontainer[i].light, Lcontainer[i].light, 0));
+        }
 
     }
 
@@ -17,32 +50,83 @@ public class StageStar2 : MonoBehaviour {
     void Update()
     {
 
-        if (GetLimitStar.clearflg != true)
+        if (GetLimitStarLine.clearflg != true)
         {
+
 
             for (int i = 0; i < this.transform.childCount; i++)
             {
-
-                if (GetLimitStar.ReceiveLimitNum <= 0)
+                if (GetLimitStarLine.ReceiveLimitNum == 0)
                 {
-                    this.transform.GetChild(i).gameObject.SetActive(false);
-                }
+                    if (this.transform.GetChild(i).gameObject.activeSelf)
+                    {
+                        if (Lcontainer[i].lightup)
+                        {
 
+                            Lcontainer[i].light += 0.1f;
+                            if (Lcontainer[i].light > 1.5f)
+                            {
+                                Lcontainer[i].lightup = false;
+                            }
+                        }
+                        else
+                        {
+                            Lcontainer[i].light -= 0.1f;
+                            if (Lcontainer[i].light < -0.5f)
+                            {
+                                Lcontainer[i].lightup = true;
+                                this.transform.GetChild(i).gameObject.SetActive(false);
+                            }
+                        }
+                    }
+                }
                 else
                 {
-                    if (i < GetLimitStar.ReceiveLimitNum + 1)
+
+
+
+                    if (i < GetLimitStarLine.ReceiveLimitNum+1)
                     {
+                        Lcontainer[i].lightup = true;
+                        Lcontainer[i].light = 1.0f;
                         this.transform.GetChild(i).gameObject.SetActive(true);
+
                     }
                     else
                     {
-                        this.transform.GetChild(i).gameObject.SetActive(false);
+                        if (this.transform.GetChild(i).gameObject.activeSelf)
+                        {
+                            if (Lcontainer[i].lightup)
+                            {
+
+                                Lcontainer[i].light += 0.1f;
+                                if (Lcontainer[i].light > 3.0f)
+                                {
+                                    Lcontainer[i].lightup = false;
+                                }
+                            }
+                            else
+                            {
+                                Lcontainer[i].light -= 0.1f;
+                                if (Lcontainer[i].light < -0.5f)
+                                {
+                                    Lcontainer[i].lightup = true;
+                                    this.transform.GetChild(i).gameObject.SetActive(false);
+                                }
+                            }
+
+
+                        }
+
                     }
                 }
+
+
+                lightmat[i].SetColor("_EmissionColor", new Color(Lcontainer[i].light, Lcontainer[i].light, 0));
+
+
             }
         }
-
-
 
 
 
