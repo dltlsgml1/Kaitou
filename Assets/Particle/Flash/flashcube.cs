c53381ce426d84083478c1e9361a96684ff8627d
WIN8OHS50173\ohs50173 <keisuke.877877@gmail.com>
2018-06-22 16:58:59 +0900

    ブロックエフェクト追加

---
+++ Assets/Particle/Flash/flashcube.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashcube : MonoBehaviour {
+
+    //GameObject Setblock;
+    Blocks SetBlock;
+    GameMain SetGameMain;
+    bool oneroot;
+	// Use this for initialization
+	void Start () {
+        oneroot = false;
+        SetBlock = GetComponentInParent<Blocks>();
+        SetGameMain = GameObject.Find("MainSceneScript").GetComponent<GameMain>();
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            this.transform.GetChild(i).gameObject.SetActive(false);
+
+        }
+        if (SetBlock.StartBlockFlg)
+        {
+                Destroy(GameObject.Find("FlashCubeParticle"));
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!SetBlock.StartBlockFlg)
+        {
+            if (oneroot)
+            {
+                Destroy(GameObject.Find("FlashCubeParticle"));
+            }
+
+
+            if (SetBlock.BurnFlg && SetGameMain.buttonup)
+            {
+                    for (int i = 0; i < this.transform.childCount; i++)
+                    {
+                        this.transform.GetChild(i).gameObject.SetActive(true);
+
+                    }
+                    oneroot = true;
+            }
+           
+        }
+
+	}
+}
