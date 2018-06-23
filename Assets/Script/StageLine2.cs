8687f0b4da84b7720b257b5441b8769c80e2ee85
WIN8OHS50173\ohs50173 <keisuke.877877@gmail.com>
2018-06-23 15:08:53 +0900

    上限回数の点滅

--- Assets/Script/StageLine2.cs
+++ Assets/Script/StageLine2.cs
@@ -4,52 +4,127 @@ using UnityEngine;
 
 public class StageLine2 : MonoBehaviour {
 
-    //public GameObject SetStarObj;
-    LifeStarRecive2 GetLimitStar;
+    public struct light_container
+    {
+        public float lightpower;
+        public bool lightswitch;
+    }
+    public Material[] lightmat = new Material[9];
+    LifeStarRecive2 GetLimitStarLine;
+    light_container[] Lcontainer = new light_container[9];
+
     // Use this for initialization
     void Start()
     {
-        GetLimitStar = GetComponentInParent<LifeStarRecive2>();
-
+        GetLimitStarLine = GetComponentInParent<LifeStarRecive2>();
+        LightLineInit();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (GetLimitStar.clearflg != true)
+        if (GetLimitStarLine.clearflg != true)
         {
-
             for (int i = 0; i < this.transform.childCount; i++)
             {
-
-                if (GetLimitStar.ReceiveLimitNum <= 0)
+                if (i < GetLimitStarLine.ReceiveLimitNum)
                 {
-                    this.transform.GetChild(i).gameObject.SetActive(false);
+                    LightLive(i);
                 }
-
                 else
                 {
-                    if (i < GetLimitStar.ReceiveLimitNum )
-                    {
-                        this.transform.GetChild(i).gameObject.SetActive(true);
-                    }
-                    else
-                    {
-                        this.transform.GetChild(i).gameObject.SetActive(false);
-                    }
+                    Light_Delete(i);
                 }
+                lightmat[i].SetColor("_EmissionColor", new Color(Lcontainer[i].lightpower, Lcontainer[i].lightpower, 0));
             }
         }
-
-
-
-
-
 
     }
 
 
+    void LightLineInit()
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            this.transform.GetChild(i).GetComponent<SpriteRenderer>().material = lightmat[i];
+           
+            if (i < GetLimitStarLine.ReceiveLimitNum)
+            {
+                this.transform.GetChild(i).gameObject.SetActive(true);
+                Lcontainer[i].lightpower = 2.0f;
+
+            }
+            else
+            {
+                this.transform.GetChild(i).gameObject.SetActive(false);
+                Lcontainer[i].lightpower = -0.5f;
+            }
+            Lcontainer[i].lightswitch = true;
+            lightmat[i].SetColor("_EmissionColor", new Color(Lcontainer[i].lightpower, Lcontainer[i].lightpower, 0));
+        }
+    }
+
+
+
+
+
+
+    void LightLive(int materialcount)
+    {
+        this.transform.GetChild(materialcount).gameObject.SetActive(true);
+        if (materialcount == (GetLimitStarLine.ReceiveLimitNum - 1))
+        {
+            LightUP_DOWN(materialcount);
+        }
+        else
+        {
+            Lcontainer[materialcount].lightpower = 2.0f;
+        }
+    }
+
+
+    void LightUP_DOWN(int materialcount)
+    {
+        if (Lcontainer[materialcount].lightswitch)
+        {
+            Lcontainer[materialcount].lightpower += 0.2f;
+            if (Lcontainer[materialcount].lightpower > 3.5f)
+            {
+                Lcontainer[materialcount].lightswitch = false;
+            }
+        }
+        else
+        {
+            Lcontainer[materialcount].lightpower -= 0.2f;
+            if (Lcontainer[materialcount].lightpower < 1.0f)
+            {
+                Lcontainer[materialcount].lightswitch = true;
+            }
+        }
+
+    }
+
+
+
+
+
+
+    void Light_Delete(int materialcount)
+    {
+        if (Lcontainer[materialcount].lightpower > 0)
+        {
+            Lcontainer[materialcount].lightpower -= 0.1f;
+            if (Lcontainer[materialcount].lightpower < 0)
+            {
+                this.transform.GetChild(materialcount).gameObject.SetActive(false);
+            }
+        }
+    }
+
+
+
+    
     //
     
 }
