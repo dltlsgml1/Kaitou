002a50ad7b078857a0c203d3ae997bbb011420a9
NotePC_Cielo <sorara.sky231@gmail.com>
2018-06-25 19:21:43 +0900

    ステージタイトルの表示作成・作業中

---
+++ Assets/StageTitle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class StageTitle : MonoBehaviour {
+
+	Renderer Render_test;
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Renderer>().material.mainTexture = Resources.Load ("Prefabs/Stage/" + PassStageID.PassStageName ()) as Texture;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
