f1cc1e424bbaad0a9388ce698169a610b80a6946
NotePC_Cielo <sorara.sky231@gmail.com>
2018-07-02 14:21:31 +0900

    exeの起動時に画面サイズが勝手に切り替わるように変更修正

---
+++ Assets/Script/ScreenSize.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize : MonoBehaviour {
+	public int ScreenWidth;
+	public int ScreenHeight;
+	// Use this for initialization
+	void  Awake() {
+		// PC向けビルドだったらサイズ変更
+		if (Application.platform == RuntimePlatform.WindowsPlayer ||
+			Application.platform == RuntimePlatform.OSXPlayer ||
+			Application.platform == RuntimePlatform.LinuxPlayer )
+		{
+			Screen.SetResolution(ScreenWidth, ScreenHeight, false);
+			Screen.fullScreen = true;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
