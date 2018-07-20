bd2d2864ec05355ae1f17fb5e1979dd898d1e3c2
otyappa <tosiboukyabetu@yshoo.co.jp>
2018-07-20 17:08:23 +0900

    フリーズしてしまうバグをどうにかしました

--- Assets/Script/FrameFade.cs
+++ Assets/Script/FrameFade.cs
@@ -9,7 +9,7 @@ public class FrameFade : MonoBehaviour {
     public static bool FadeOutFlag = false;
     bool FadeInit = false;
     float alfa = 0;
-    float time = 0.3f;
+    float time = 0.1f;
     float red, green, blue;
 
     // Use this for initialization
